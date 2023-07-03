4857836778741735e27bddd59c9a46f66eb448d9
sanieni6 <luissanchezzamora23@gmail.com>
2023-07-03 14:23:31 -0500

    fix rubocop linters

--- app/models/group_operation.rb
+++ app/models/group_operation.rb
@@ -1,4 +1,4 @@
 class GroupOperation < ApplicationRecord
-    belongs_to :group, foreign_key: :group_id
-    belongs_to :operation, foreign_key: :operation_id
+  belongs_to :group, foreign_key: :group_id
+  belongs_to :operation, foreign_key: :operation_id
 end
