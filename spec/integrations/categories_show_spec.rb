d0649ec3d6a852893f974adad0228cf6decb3d40
sanieni6 <luissanchezzamora23@gmail.com>
2023-07-05 21:32:15 -0500

    Add integration tests

---
+++ spec/integrations/categories_show_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Integrations tests for categories/show', type: :feature do
+  before(:each) do
+    @user = User.create(name: 'luis', email: 'example@email.com', password: 'password')
+    @group = Group.create(name: 'group',
+                          icon: 'https://img.uxwing.com/wp-content/themes/uxwing/download/food-drinks-cooking/meal-food-icon.png',
+                          author_id: @user.id)
+    @operation = Operation.create(name: 'pizza', amount: 12, author_id: @user.id)
+    @group_operation = GroupOperation.create(group_id: @group.id, operation_id: @operation.id)
+  end
+
+  describe 'GET /show' do
+    before do
+      login_as(@user, scope: :user)
+      visit group_path(@group)
+    end
+
+    it 'should render the title of the page' do
+      expect(page).to have_content('Transactions')
+    end
+
+    it 'should render the name of the group' do
+      expect(page).to have_content('pizza')
+    end
+
+    it 'should display a link to create a new Transaction' do
+      expect(page).to have_link('New Transaction', href: new_group_operation_path(@group))
+    end
+  end
+end
